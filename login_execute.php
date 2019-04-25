859a19335c4dc6aeb1ce278e03d178075dc76287
Sofi Yaremenko <sofia@yaremenko.ws>
2019-04-26 02:36:13 +0300

    2 36

--- login_execute.php
+++ login_execute.php
@@ -14,6 +14,14 @@ if (isset($_POST['password']))
 $query = "SELECT * FROM users WHERE email='$email'";
 $result = mysqli_query($db, $query);
 $user;
+if(mysqli_num_rows($result) == 0){
+          die("This username could not be found! ");
+      }
+      while ($row = mysqli_fetch_array($result)){
+          $pass = $row['password'];
+        
+         
+      }
 /*while($row = mysqli_fetch_assoc($result)){
 	$user = new User($row["email"], $row["password"], $row["name"], $row["surname"],$row["img_profile"], $row["about"]);
 }
@@ -22,7 +30,7 @@ if (!$result || !isset($user)){
 	unset($email);
 	header("location: ./examples/login-page.html");
 }else{*/
-	if($user->getPassword() == $password){
+	if($pass == $password){
 		session_start();
 		$_SESSION["loggedin"] = true;
 		$_SESSION["email"] = $email;
